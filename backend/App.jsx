3119bb4636ee0109947142c89cd6572f9d7b96b3
Ashbipbinu <abinu@thoughtlinetech.com>
2024-09-05 11:01:27 +0530

    Merged changes from otherbranch into backend folder

---
+++ backend/App.jsx
@@ -0,0 +1,27 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage  from "./pages/home/HomePage";
+import LoginPage  from "./pages/auth/login/LoginPage";
+import SignUpPage from "./pages/auth/signup/SignUp";
+import Sidebar from "./components/common/SideBar";
+import RightPanel from "./components/common/RightPanel";
+import ProfilePage from './pages/profile/ProfilePage';
+import NotificationPage from './pages/notification/NotificationPage';
+
+function App() {
+
+  return (
+    <div className='flex max-w-6xl mx-auto'>
+      <Sidebar/>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path="/profile/:username" element={<ProfilePage/>}/>
+        <Route path="/notifications" element={<NotificationPage/>}/>
+      </Routes>
+      <RightPanel/>
+    </div>
+  );
+}
+
+export default App;
